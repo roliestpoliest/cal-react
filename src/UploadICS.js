962ba9f571197b8f33f9f4b17bf39f62ef850412
carolyn <41408998+roliestpoliest@users.noreply.github.com>
2023-07-20 13:25:38 -0400

    added local storage support

--- src/UploadICS.js
+++ src/UploadICS.js
@@ -17,47 +17,51 @@ class UploadICS extends Component {
     reader.onload = async (e) => { 
     const text = (e.target.result)
     console.log(text)
-//   alert(text)
-// BEGIN:VEVENT
+    // alert(text)
+    // BEGIN:VEVENT
+
     const lines = text.split("\n");
     let start = "null"
     let end = "null"
     let summary = "null"
     let location = "null"
     let description = "null"
-    
+
+    let holdLocalStorage = []
+    holdLocalStorage = JSON.parse(localStorage.getItem('events')) || [];
+      
     for(let i = 0; i < lines.length; i++){
         //DTSTART, DTEND, SUMMARY, LOCATION
         if(i < lines.length && lines[i].indexOf("END:VEVENT") == 0){
+          console.log("start: " + start);
+          let year = parseInt(start.slice(0, 4));
+          let month = parseInt(start.slice(4, 6));
+          let day = parseInt(start.slice(6, 8));
+          let hour = parseInt(start.slice(9, 11));
+          let minute = parseInt(start.slice(11, 13));
+          let second = parseInt(start.slice(13, 15));
+          let startDate = new Date(year, month, day, hour, minute, second);
+          console.log("start: " + startDate);
 
-            console.log("start: " + start);
-            let year = parseInt(start.slice(0, 4));
-            let month = parseInt(start.slice(4, 6));
-            let day = parseInt(start.slice(6, 8));
-            let hour = parseInt(start.slice(9, 11));
-            let minute = parseInt(start.slice(11, 13));
-            let second = parseInt(start.slice(13, 15));
-            let startDate = new Date(year, month, day, hour, minute, second);
-            console.log("start: " + startDate);
+          console.log("end: " + end);
+          year = parseInt(end.slice(0, 4));
+          month = parseInt(end.slice(4, 6));
+          day = parseInt(end.slice(6, 8));
+          hour = parseInt(end.slice(9, 11));
+          minute = parseInt(end.slice(11, 13));
+          second = parseInt(end.slice(13, 15));
+          let endDate = new Date(year, month, day, hour, minute, second);
+          console.log("end: " + endDate);
 
-            console.log("end: " + end);
-            year = parseInt(end.slice(0, 4));
-            month = parseInt(end.slice(4, 6));
-            day = parseInt(end.slice(6, 8));
-            hour = parseInt(end.slice(9, 11));
-            minute = parseInt(end.slice(11, 13));
-            second = parseInt(end.slice(13, 15));
-            let endDate = new Date(year, month, day, hour, minute, second);
-            console.log("end: " + endDate);
+          var diff =(startDate.getTime() - endDate.getTime()) / 1000;
+          diff /= 60;
+          let duration = Math.abs(Math.round(diff));
+          console.log("duration: " + duration)
 
-            var diff =(startDate.getTime() - endDate.getTime()) / 1000;
-            diff /= 60;
-            let duration = Math.abs(Math.round(diff));
-            console.log("duration: " + duration)
-
-            let calendarInstance = new calendarInfo(start, end, summary, location, description, duration);
-            this.state.calendarEvents.push(calendarInstance);
-            console.log(calendarInstance);
+          let calendarInstance = new calendarInfo(start, end, summary, location, description, duration);
+          this.state.calendarEvents.push(calendarInstance);
+          holdLocalStorage.push(calendarInstance)
+          console.log(calendarInstance);
         }
 
         if(lines[i].indexOf("DTSTART") == 0){
@@ -77,6 +81,12 @@ class UploadICS extends Component {
         }
     }
     console.log(this.state.calendarEvents);
+    localStorage.setItem("events", JSON.stringify(holdLocalStorage));
+    // calendarEvents
+    
+    // a.push = a.concat(this.state.calendarEvents);
+    // alert(a);
+    // localStorage.setItem('events', JSON .stringify(a));
     };
     reader.readAsText(e.target.files[0])
   }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import CalendarDisplay from './CalendarDisplay.js'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+localStorage.setItem("events", JSON.stringify([]));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
@@ -15,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
